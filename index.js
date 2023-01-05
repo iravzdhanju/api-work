12161ff10046c472d0b192e485c0735d116ea0b7
Ravinderpal Singh <52969271+iravzdhanju@users.noreply.github.com>
2023-01-05 01:14:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 
-// Create an instance of the Express app
 const app = express();
+
+app.use(cors());
 
 let result = 2021;
 
-// Set up an interval to update the result every 5 seconds
 setInterval(() => {
   result -= 5;
 }, 5000);
 
-// Set up a route that listens for GET requests on the root path ('/')
 app.get('/', (req, res) => {
-  // Send the result as a JSON object in the response
   res.json({ result: result });
 });
 
-// Start the server on port 3000
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+exports.api = app;
